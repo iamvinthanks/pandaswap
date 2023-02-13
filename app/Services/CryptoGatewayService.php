652b9b75833a2a838444891e3a92c1320f88000c
pemulungtoken <pemulungtoken@gmail.com>
2023-02-13 22:56:57 +0700

    Refactory

--- app/Services/CryptoGatewayService.php
+++ app/Services/CryptoGatewayService.php
@@ -10,24 +10,11 @@ use Str;
 use DB;
 use IEXBase\TronAPI\Provider\HttpProvider;
 
-
-
 class CryptoGatewayService
 {
     public function Create($amount)
     {
-        $fullNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.shasta.trongrid.io');
-        $solidityNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.shasta.trongrid.io');
-        $eventServer = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.shasta.trongrid.io');
-        try {
-            $tron = new \IEXBase\TronAPI\Tron($fullNode, $solidityNode, $eventServer);
-        } catch (\IEXBase\TronAPI\Exception\TronException $e) {
-            exit($e->getMessage());
-        }
-        $expired = Carbon::now()->addMinutes(5);
-        $createAddrs = $tron->createAccount();
-        $addressData = $createAddrs->response;
-        $coin = 'TRX';
+        
         DB::beginTransaction();
         try{
             $data = [
--- app/Services/TronService.php
+++ app/Services/TronService.php
@@ -14,5 +14,19 @@ use IEXBase\TronAPI\Provider\HttpProvider;
 
 class TronService
 {
-
+    public function generateWallet()
+    {
+        $fullNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.shasta.trongrid.io');
+        $solidityNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.shasta.trongrid.io');
+        $eventServer = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.shasta.trongrid.io');
+        try {
+            $tron = new \IEXBase\TronAPI\Tron($fullNode, $solidityNode, $eventServer);
+        } catch (\IEXBase\TronAPI\Exception\TronException $e) {
+            exit($e->getMessage());
+        }
+        $expired = Carbon::now()->addMinutes(5);
+        $createAddrs = $tron->createAccount();
+        $addressData = $createAddrs->response;
+        $coin = 'TRX';
+    }
 }
